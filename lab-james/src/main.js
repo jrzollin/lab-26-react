8f8b62297de4221e464731b71067797502678961
jrzollin <jrzollin@yahoo.com>
2017-12-02 12:32:39 -0800

    finished cowsay added cow options

--- lab-james/src/main.js
+++ lab-james/src/main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './style/main.scss';
+import faker from 'faker';
+import {say, SQUIRREL, DEFAULT, VADER, SKELETON} from 'cowsay';
 
 class Header extends React.Component {
 
@@ -19,12 +21,61 @@ class Header extends React.Component {
 }
 
 class App extends React.Component {
-  
+
   constructor(props){
     super(props);
 
+    this.handleCowSay = this.handleCowSay.bind(this);
+    this.updateStateSay = this.updateStateSay.bind(this);
+    this.updateStateCow = this.updateStateCow.bind(this);
+    this.chooseCowReg = this.chooseCowReg.bind(this);
+    this.chooseCowDarth = this.chooseCowDarth.bind(this);
+    this.chooseCowSkeleton = this.chooseCowSkeleton.bind(this);
 
+    this.state = {
+      content: 'Moo',
+      cow: DEFAULT
+    };
+
+  }
+
+  handleCowSay(e){
+    this.updateStateSay(this.state.content);
+  }
+
+  updateStateSay(content){
+    let comment = faker.fake('{{lorem.words}}');
+    this.setState({content: comment});
+  }
+
+  chooseCowReg(e){
+    this.updateStateCow(DEFAULT);
+  }
+
+  chooseCowDarth(e){
+    this.updateStateCow(VADER);
+  }
+
+  chooseCowSkeleton(e){
+    this.updateStateCow(SKELETON);
+  }
+
+  updateStateCow(choice){
+    this.setState({cow: choice});
+  }
+
+  render(){
+    return (
+      <div>
+        <Header />
+        <pre>{say({text: this.state.content, cow: this.state.cow})}</pre>
+        <button onClick={this.handleCowSay}>Poke the cow</button>
+        <button onClick={this.chooseCowReg}>Regular Cow</button>
+        <button onClick={this.chooseCowDarth}>Vader Cow</button>
+        <button onClick={this.chooseCowSkeleton}>Skeleton Cow</button>
+      </div>
+    )
   }
 }
 
-ReactDom.render(<Header/>, document.getElementById('root'));
+ReactDom.render(<App/>, document.getElementById('root'));
